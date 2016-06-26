ac7e0dc11a6d1e2b1cccf8fee789ba3074a75af8
agabrielsen <a.gabrielsen01@gmail.com>
2016-06-26 22:04:10 +0100

    Update is_installed.R

--- is_installed.R
+++ is_installed.R
@@ -1,10 +1,8 @@
 #' Examines if a package is already installed
 #'
-#' Examines if the package is already present
 #' @export
 #' @examples 
 #' \\dontrun{
-#' is_installed("GlobalVAR")}
 #' }
 
 is_installed <- function(mypkg) is.element(mypkg, installed.packages()[,1])
